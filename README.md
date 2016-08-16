13ea197336104eb0d8b4ae947d61b7877a417922
meglad <wangyu5577@gmail.com>
2016-08-17 00:47:05 +0800

    增加 html 压缩功能

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # jhtmls-loader
 --------------------------------
+[![npm](https://img.shields.io/npm/v/jhtmls-loader.svg)](https://www.npmjs.com/package/jhtmls-loader)
 
-[jhtml](https://github.com/zswang/jhtmls) 模版的 webpack 预编译 loader
+[jhtmls](https://github.com/zswang/jhtmls) 模版的 webpack 预编译 loader
 
 ## 安装
 
 $ npm install jhtmls-loader
+
+## 说明 (模版具体用法请看 [https://github.com/zswang/jhtmls](https://github.com/zswang/jhtmls))
+
+```js
+var data = {...};
+var render = require('jhtmls!./template.html');   // 返回渲染函数
+document.body.innerHTML = render(data);
+```
 
 ### LICENSE MIT
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var loaderUtils = require("loader-utils");
+var htmlMinifier = require("html-minifier");
 var jhtmls = require('jhtmls');
 
 var _jhtmls = function (template, isWith) {
@@ -14,6 +15,7 @@ var _jhtmls = function (template, isWith) {
     ";
 
   function encodeHTML(text) {
+    text = typeof text === 'undefined' ? '' : text;
     return String(text).replace(/["<>& ]/g, function (all) {
       return '&' + htmlEncodeDict[all] + ';';
     });
@@ -51,10 +53,34 @@ var _jhtmls = function (template, isWith) {
 module.exports = function (source) {
   if (this.cacheable) this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
+  // 压缩
 
-  var module = {};
-  eval(source);
+  var minimizeOptions = Object.assign({}, query);
+  if (typeof query.minimize === "boolean" ? query.minimize : this.minimize) {
+    [
+      "removeComments",
+      "removeCommentsFromCDATA",
+      "removeCDATASectionsFromCDATA",
+      "collapseWhitespace",
+      "conservativeCollapse",
+      //"removeAttributeQuotes",
+      "useShortDoctype",
+      "keepClosingSlash",
+      "minifyJS",
+      //"minifyCSS",
+      "removeScriptTypeAttributes",
+      "removeStyleTypeAttributes",
+      "preserveLineBreaks"
+    ].forEach(function (name) {
+      if (typeof minimizeOptions[name] === "undefined") {
+        minimizeOptions[name] = true;
+      }
+    });
+    source = htmlMinifier.minify(source, minimizeOptions);
+  }
 
-  source = _jhtmls(module.exports, query.with) + 'module.exports = render;';
+  source = source.replace(/(\n\r)|(\r\n)|(\r)|(\n)/g, '\n\r');
+  source = _jhtmls(source, query.with) + 'module.exports = render;';
   return source;
 };
+
